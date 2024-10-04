4e4914335a1944c85e6f6e4dcc930d955a1d5134
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-03 21:01:37 -0500

    feat(main): implement main view structure and behavior.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {LogoApiService} from './shared/services/logo-api.service';
+import {NgForOf, NgIf} from '@angular/common';
+import {MatAnchor} from '@angular/material/button';
+import {MatToolbar} from '@angular/material/toolbar';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink, NgIf, NgForOf, TranslateModule, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'upcpre202402si729sw56eau202213358';
+export class AppComponent implements OnInit{
+  title = 'Eventify';
+  logoUrl: string = '';
+  options = [
+    { title: 'Toolbar.home', path: '/home' },
+    { title: 'Toolbar.RatingCheck', path: '/engagement/ratings/new' }
+  ];
+
+  constructor(private logoApiService: LogoApiService,translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('en');
+  }
+
+  ngOnInit(): void {
+    const domain = 'eventify.io';
+    this.logoUrl = this.logoApiService.getUrlToLogo(domain);
+  }
 }
