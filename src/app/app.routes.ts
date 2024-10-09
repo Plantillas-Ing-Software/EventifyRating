0fa2552f3efb3c94e75b9db2f603a219c5b3362e
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-09 18:45:47 -0500

    feat(in-app-navigation): edit setup in-app navigation rules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
-import {RatingCheckedComponent} from './registration/components/rating-checked/rating-checked.component';
+import { EventRatingComponent } from './engagement/component/event-rating/event-rating.component';
 import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'home',             component: HomeComponent },
-  {path:'engagement/ratings/new',component:RatingCheckedComponent},
+  {path:'engagement/ratings/new',component:EventRatingComponent},
   { path: '',                 redirectTo: 'home', pathMatch: 'full' },
   { path: '**',               component: PageNotFoundComponent }
 ];
